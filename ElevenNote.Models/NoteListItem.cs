01aa96c8995cf368c1a4b15b743b38857ce6d8a5
Kate Thomas <kpthomas6@hotmail.com>
2018-11-24 22:28:04 -0500

    note list item model

--- ElevenNote.Models/NoteListItem.cs
+++ ElevenNote.Models/NoteListItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ namespace ElevenNote.Models
         public int NoteID { get; set; }
         public string Title { get; set; }
 
+        [Display(Name="Created")]
         public DateTimeOffset CreatedUtc { get; set; }
 
         public override string ToString()
--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ElevenNote.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
