1c566b755f49d4f972f30de9f47385045d3e4ca1
Kate Thomas <kpthomas6@hotmail.com>
2018-11-24 23:43:12 -0500

    updated services

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -7,12 +7,29 @@ using System.Web.Mvc;
 
 namespace ElevenNote.WebMVC.Controllers
 {
+    [Authorize]
     public class NoteController : Controller
     {
         // GET: Note
         public ActionResult Index()
         {
             var model = new NoteListItem[0];
+            return View(model);
+        }
+        //GET
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(NoteCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
             return View(model);
         }
     }
