6fd02f222e48b61b00b6f96b45e5190e143b8296
Kate Thomas <kpthomas6@hotmail.com>
2018-11-26 12:59:40 -0500

    dictionary

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -2,9 +2,6 @@
 using ElevenNote.Services;
 using Microsoft.AspNet.Identity;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace ElevenNote.WebMVC.Controllers
@@ -33,12 +30,13 @@ namespace ElevenNote.WebMVC.Controllers
         {
             if (!ModelState.IsValid) return View(model);
 
-            // var service = CreateNoteService();
+            var service = CreateNoteService();
 
-            //if(service.CreateNote(model))
-            // {
-            // return RedirectToAction("Index");
-            // };
+            if (service.CreateNote(model))
+            {
+                TempData["SaveResult"] = "Your note was created.";
+                return RedirectToAction("Index");
+            };
             ModelState.AddModelError("", "Note could not be created.");
 
             return View(model);
